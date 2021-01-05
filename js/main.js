6824061a89596ab1ce71bd54c7fcf292a08ceaf2
Rizky Abraham <rizlist2020@gmail.com>
2021-01-04 22:30:02 -0800

    Add js files and index files for week 1 & 2

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const links = [
+    {
+      label: "Week 1 Notes",
+      url: "week1/index.html"
+    },
+    {
+        label: "Week 2 Notes",
+        url: "week2/index.html"
+    }
+  ]
+
+function populateUl(){
+  ol = document.createElement('ol');
+
+  document.getElementById('myItemList').appendChild(ol);
+
+  links.forEach(function (link) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    ol.appendChild(li);
+    li.appendChild(a);
+
+    a.innerHTML += link.label;
+    a.setAttribute('href', link.url);
+
+  });
+};
+
+
+
