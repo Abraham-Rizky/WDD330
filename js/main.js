fb311796e320e0ba8544f8bb36feb7a8643c303d
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2021-01-17 21:21:33 -0800

    Completed week 3 reading

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ const links = [
     {
         label: "Week 2 Notes",
         url: "week2/index.html"
-    }
+    },
+    {
+      label: "Week 3 Notes",
+      url: "week3/index.html"
+  }
   ]
 
 function populateUl(){
