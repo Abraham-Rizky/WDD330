a1beafc361e7cc5d0daece3d3726760925965f0f
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2021-02-02 23:03:21 -0800

    Added functionality

--- todo/js/main.js
+++ todo/js/main.js
@@ -3,5 +3,18 @@ import Tasks from './todo.js';
 //on load, grab the array and insert it into the page
 const myTask = new Tasks('tasks');
 window.addEventListener('load', () => {
-  myHike.showTaskList();
+  myTask.showTaskList();
+  myTask.addTabListeners();
+});
+
+const textbox = document.getElementById('textbox');
+const addTask = document.getElementById('addTask');
+addTask.addEventListener('click', () => {
+  myTask.addToDo();
+});
+textbox.addEventListener('keyup', function(event) {
+  if(event.keyCode === 13) {
+    event.preventDefault();
+    addNew.click();
+  }
 });
