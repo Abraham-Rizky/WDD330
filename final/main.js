4abdcbf4e90a524e0d1291d8caf739412360ef82
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2021-02-22 23:05:49 -0800

    Added pokemon final project

--- final/main.js
+++ final/main.js
@@ -3,5 +3,5 @@ import Pokemon from './pokemon.js';
 //on load, grab the array and insert it into the page
 const pokemon = new Pokemon('starwars', 'people');
 window.addEventListener('load', () => {
-  swPeople.init();
+  pokemon.init();
 });
