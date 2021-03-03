72f22172080d093a4d8927226c7cb56a252c6e59
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2021-03-02 21:23:48 -0800

    updated final project file

--- final/main.js
+++ final/main.js
@@ -1,7 +1,9 @@
-import pokedexList from './pokemon.js';
+console.log('about to fetch a list of Star Wars characters');
+
+import StarWarsList from './starwars.js';
 
 //on load, grab the array and insert it into the page
-const pokedex = new pokedexList('pokedex', 'pokemon');
+const swPeople = new StarWarsList('starwars', 'people');
 window.addEventListener('load', () => {
-  pokedex.init();
+  swPeople.init();
 });
